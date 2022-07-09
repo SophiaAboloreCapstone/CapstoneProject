4248de038c508d54e6489d22422aa4fd2f7d7c14
Opebiyi Abolore <sophiaabolore@fb.com>
2022-07-08 18:48:43 -0700

    Removed all and now obsolete files from the repo

--- src/components/MatchGrid/MatchGrid.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react"
-import "./MatchGrid.css"
-export default function MatchGrid() {
-    return (
-        <div className="match-grid">
-            <h1>Here are your matches!</h1>
-        </div>
-    )
-}

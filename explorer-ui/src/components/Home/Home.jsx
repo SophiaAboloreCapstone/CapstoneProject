f4212d43c040884415ecb8ef44df1c9f99994cf2
Opebiyi Abolore <sophiaabolore@fb.com>
2022-07-19 15:34:53 -0700

    Added slideshow and footer mockup to the homepage

--- explorer-ui/src/components/Home/Home.jsx
+++ explorer-ui/src/components/Home/Home.jsx
@@ -2,12 +2,16 @@ import * as React from "react"
 import "./Home.css"
 import Hero from "./Hero/Hero"
 import NavBar from "../NavBar/NavBar"
+import Testimonials from "../Testimonials/Testimonials"
+import Footer from "./Footer/Footer"
 // import Hero from "../Hero/Hero"
 export default function Home() {
     return (
         <div className="home">
             <NavBar />
             <Hero />
+            <Testimonials />
+            <Footer />
         </div>
     )
 }
